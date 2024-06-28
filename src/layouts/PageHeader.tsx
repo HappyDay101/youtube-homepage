63a837ac67c55a176377f3c1ac9ae121c91a63d1
Osama Suleiman <osama.suleiman97@gmail.com>
2024-06-29 01:13:47 +0900

    Create header search section

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,10 +1,11 @@
-import { Bell, Menu, Upload, User } from "lucide-react";
+import { Bell, Menu, Mic, Search, Upload, User } from "lucide-react";
 import { Button } from "../components/Button";
 import logo from "../assets/logo.png";
 
 export function PageHeader() {
   return (
     <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
+      {/* Left side of header */}
       <div className="flex gap-4 items-center	flex-shrink-0">
         <Button variant="ghost" size="icon" className="m-10">
           <Menu />
@@ -13,8 +14,26 @@ export function PageHeader() {
           <img src={logo} alt="Logo" className="h-6" />
         </a>
       </div>
-      <div></div>
-      <div>
+
+      {/* mid-section of header */}
+      <form className="flex gap-4 flex-grow justify-center">
+        <div className="flex flex-grow max-w-[600px]">
+          <input
+            type="search"
+            placeholder="Search"
+            className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
+          />
+          <Button>
+            <Search />
+          </Button>
+        </div>
+        <Button type="button" size="icon" className="flex-shrink-0">
+          <Mic />
+        </Button>
+      </form>
+
+      {/* Right Side of header */}
+      <div className="flex flex-shrink-0 md:gap-2">
         <Button size="icon" variant="ghost">
           <Upload />
         </Button>
